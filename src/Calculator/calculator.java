b3257af93bc3ad3f95505ed41e3b62bc1ea5e8f3
ArturMedetov007 <medetovartur65@gmail.com>
2023-11-25 23:34:20 +0600

    3 zadanie

---
+++ src/Calculator/calculator.java
@@ -0,0 +1,10 @@
+package Calculator;
+
+public class calculator {
+    public void test(int a, int b) {
+        System.out.println(a + b); }
+    public void test(int a, double b) {
+        System.out.println(a + b); }
+    public void test(double a ,double b) {
+        System.out.println(a + b); }
+}
---
+++ src/Calculator/test3.java
@@ -0,0 +1,10 @@
+package Calculator;
+
+public class test3 {
+    public static void main(String[] args) {
+        calculator c1 = new calculator();
+        c1.test(3,5);
+        c1.test(2,2.5);
+        c1.test(2.3,5.3);
+    }
+}
